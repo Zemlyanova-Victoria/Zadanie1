46f5dce5ac91311db8966daa19febc3b896030f5
Zemlyanova-Victoria <65289097+Zemlyanova-Victoria@users.noreply.github.com>
2020-05-14 19:51:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ViLibrary;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(lib1.stpow("ha", 6));
+            Console.WriteLine(lib1.rev("love"));
+            Console.WriteLine(lib1.remov("uLoveappur", "u"));
+            Console.WriteLine(lib1.leng("love forever!"));
+        }
+    }
+}
---
+++ lib1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ViLibrary
+{
+    public class lib1
+    {
+        static public string stpow(string k, int b)
+        {
+            string k1 = k;
+            for (int s = 0; s < b - 1; s++)
+            {
+                k = k + k1;
+            }
+            return (k);
+        }
+        static public string rev(string a)
+        {
+            string l = "";
+            for (int s = a.Length - 1; s > -1; s--)
+            {
+                l = l + a[s] + "";
+            }
+            return (l);
+        }
+        static public string remov(string y, string u)
+        {
+            for (int s = 0; s < y.Length; s++)
+            {
+                y = y.Replace(u, "");
+            }
+            return (y);
+        }
+        static public int leng(string f)
+        {
+            int x = f.Length;
+            return (x);
+        }
+    }
+}
+
